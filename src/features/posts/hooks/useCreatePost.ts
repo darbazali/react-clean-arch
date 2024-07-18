73cf680525ce274bfc54b366fedadc15d709f027
Interceptor <darbaz.me@gmail.com>
2024-07-19 00:41:55 +0300

    Add useCreatePost hook and createPost method in postService for creating new posts

---
+++ src/features/posts/hooks/useCreatePost.ts
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { Post } from "../../../core/models/Post";
+import { postService } from "../services/postService";
+
+export const useCreatePost = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [data, setData] = useState<Post>();
+
+  const createPost = async (post: Post) => {
+    try {
+      setLoading(true);
+      const data = await postService.createPost(post);
+      setData(data);
+      setLoading(false);
+    } catch (error) {
+      setError(true);
+      setLoading(false);
+    }
+  };
+
+  return { loading, error, createPost, post: data };
+};
--- src/features/posts/services/postService.ts
+++ src/features/posts/services/postService.ts
@@ -16,4 +16,16 @@ export const postService = {
     const data = await response.json();
     return data;
   },
+
+  createPost: async (post: Post): Promise<Post> => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify(post),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+    const data = await response.json();
+    return data;
+  },
 };
