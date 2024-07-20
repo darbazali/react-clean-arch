c3728adef50084f8ad770661b9bff0d9c4233dee
Interceptor <darbaz.me@gmail.com>
2024-07-20 11:10:05 +0300

    Add Accept and Authorization headers to apiAgent configuration

--- src/adapters/apiAgent.ts
+++ src/adapters/apiAgent.ts
@@ -4,20 +4,7 @@ export const apiAgent: AxiosInstance = axios.create({
   baseURL: "https://jsonplaceholder.typicode.com/",
   headers: {
     "Content-type": "application/json; charset=UTF-8",
+    Accept: "application/json",
+    Authorization: "Bearer " + "token_from_state",
   },
 });
-
-// export const authorizedFetch = (
-//   input: string | URL,
-//   config?: AxiosRequestConfig,
-// ): Promise<AxiosResponse> => {
-//   const headers = config?.headers ?? {};
-//   return apiAgent.request({
-//     ...config,
-//     url: input,
-//     headers: {
-//       ...headers,
-//       Authorization: "Bearer " + "token_from_state",
-//     },
-//   });
-// };
