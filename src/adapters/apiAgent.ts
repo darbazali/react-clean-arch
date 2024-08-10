5839d9fd0d00910c5db163d4d1eef9caf927032e
Interceptor <darbaz.me@gmail.com>
2024-08-10 11:48:34 +0300

    Refactor: use template literal for Authorization header in apiAgent

--- src/adapters/apiAgent.ts
+++ src/adapters/apiAgent.ts
@@ -1,10 +1,11 @@
 import axios, { AxiosInstance } from "axios";
+const TOKEN = "token_from_state";
 
 export const apiAgent: AxiosInstance = axios.create({
   baseURL: "https://jsonplaceholder.typicode.com/",
   headers: {
     "Content-type": "application/json; charset=UTF-8",
     Accept: "application/json",
-    Authorization: "Bearer " + "token_from_state",
+    Authorization: `Bearer ${TOKEN}`,
   },
 });
